396b280b3294e8abc52348500c862cf1e76ba9ac
Shreya Kolekar <shreyakolekar.official@gmail.com>
2024-06-05 14:50:41 -0500

    install js debugger and put node_modules to gitignore

--- app.js
+++ app.js
@@ -5,4 +5,4 @@ const routes = require('./routes');
 // executes function stored in routes for incoming requests
 const server = http.createServer(routes);
 
-server.listen(3000);
+server.listen(5000);
