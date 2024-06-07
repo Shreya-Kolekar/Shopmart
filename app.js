076f5965b6cab03d39438f2b2f8b6a72dacf33d8
Shreya Kolekar <shreyakolekar.official@gmail.com>
2024-06-07 18:27:08 -0500

    add package-json files

--- app.js
+++ app.js
@@ -1,8 +1,30 @@
-const http = require('http');
+// path module from nodejs
+const path = require('path');
 
-const routes = require('./routes');
+const express = require('express');
+const app = express();
 
-// executes function stored in routes for incoming requests
-const server = http.createServer(routes);
+// add body parser
+const bodyParser = require('body-parser');
 
-server.listen(5000);
+//routes imported
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded( {extended: false}));
+
+// static serving of css files: built-in static module of express
+app.use(express.static(path.join(__dirname, 'public')));
+
+// routes assigned
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+// route for Error Page. Any route that isn't defined.
+app.use( (req, res, next) => {
+    res.status(400).sendFile(path.join(__dirname, 'views', '404.html'));
+});
+
+// create a server and listen.
+app.listen(5000);
+
