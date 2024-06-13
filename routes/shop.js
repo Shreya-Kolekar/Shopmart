62cd7204a8758cfd6cdeec9fd8bb4d11481ba098
Shreya Kolekar <shreyakolekar.official@gmail.com>
2024-06-13 18:15:38 -0500

    Add cart, order, add product and admin product nav item and their routes

--- routes/shop.js
+++ routes/shop.js
@@ -1,12 +1,26 @@
 // path module from nodejs
 const path = require('path');
-const productsController = require('../controllers/products');
+const shopController = require('../controllers/shop');
 
 const express = require('express');
 const router = express.Router();
 
 
 // shop homepage 
-router.get('/', productsController.getProducts);
+router.get('/', shopController.getIndex);
+
+// 
+router.get('/products', shopController.getProducts);
+
+// 
+router.get('/cart', shopController.getCart);
+
+// 
+router.get('/orders', shopController.getOrders);
+
+// 
+router.get('/checkout', shopController.getCheckout);
+
+
 
 module.exports = router;
