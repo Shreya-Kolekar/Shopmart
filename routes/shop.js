6e6735220a7df5a000fccb9fb8e7c28bad623303
Shreya Kolekar <shreyakolekar.official@gmail.com>
2024-07-11 19:20:48 -0500

    Add Edit and Delete product functionality

--- routes/shop.js
+++ routes/shop.js
@@ -6,14 +6,20 @@ const express = require('express');
 const router = express.Router();
 
 
-// shop homepage 
+// get shop homepage 
 router.get('/', shopController.getIndex);
 
 // 
 router.get('/products', shopController.getProducts);
 
+// get product details
+router.get('/products/:productId', shopController.getProduct);
+
 // 
 router.get('/cart', shopController.getCart);
+
+//
+router.post('/cart', shopController.postCart);
 
 // 
 router.get('/orders', shopController.getOrders);
